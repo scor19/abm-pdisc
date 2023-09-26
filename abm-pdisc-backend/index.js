c03ce43c6f401bad5020ffc6a71dbbb8cd893254
scor19 <roccobazo@gmail.com>
2023-09-26 09:18:15 -0300

    a

--- abm-pdisc-backend/index.js
+++ abm-pdisc-backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import empleadosRouter from "./empleados.js";
+import loginRouter from "./login.js";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -8,12 +11,21 @@ app.use(express.json());
 app.use(cors());
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.json("esto es el backend");
 });
 
 app.use("/empleados", empleadosRouter);
+app.use("/auth", loginRouter);
 
 app.use((err, req, res, next) => {
   // Errores globales
--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import "./App.css";
 import Container from "./components/Container";
 import Consultar from "./components/Consultar";
 import Editar from "./components/Editar";
+import Login from "./components/Login";
+import Register from "./components/Register";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Container />} />
+        <Route path="container" element={<Container/>} />
+        <Route path="/" element={<Login />} />
         <Route path="consulta" element={<Consultar />} />
         <Route path="editar/:id" element={<Editar />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
